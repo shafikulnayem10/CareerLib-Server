4db1a715f2641e65905649c460f3b05de286e6dd
shafikulnayem10 <inayem98@gmail.com>
2026-06-08 01:21:43 +0600

    added aplicatioms get a[i

--- index.js
+++ index.js
@@ -77,6 +77,18 @@ async function run() {
             res.send(result);
         })
         //application related apis
+             app.get('/api/applications', async (req, res) => {
+            const query = {};
+            if (req.query.applicantId) {
+                query.applicantId = req.query.applicantId;
+            }
+            if (req.query.jobId) {
+                query.jobId = req.query.jobId;
+            }
+            const cursor = applicationsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
             app.post('/api/applications', async (req, res) => {
             const application = req.body;
             const newApplication = {
